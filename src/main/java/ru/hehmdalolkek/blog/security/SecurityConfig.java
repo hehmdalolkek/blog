03b6743fe980bca9ab8a4ca20d3d5d4140fda760
hehmdalolkek <badeha.inna@yandex.ru>
2024-07-07 15:47:11 +0400

    configured form login

---
+++ src/main/java/ru/hehmdalolkek/blog/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package ru.hehmdalolkek.blog.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers("/", "/error", "/webjars/**").permitAll()
+                        .requestMatchers("/login", "/registration").anonymous()
+                        .anyRequest().authenticated())
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/", false)
+                        .failureUrl("/login?error"))
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .deleteCookies("JSESSIONID")
+                        .logoutSuccessUrl("/login?logout"))
+                .rememberMe(rememberMe -> rememberMe
+                        .key("SuperSecretKey")
+                        .tokenValiditySeconds(9999))
+                .build();
+    }
+
+}
