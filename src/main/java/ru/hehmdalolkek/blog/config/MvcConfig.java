517ad1a3f85c7a0e98b9437c567f0734feab1009
hehmdalolkek <badeha.inna@yandex.ru>
2024-07-09 23:39:22 +0400

    added index page

--- src/main/java/ru/hehmdalolkek/blog/config/MvcConfig.java
+++ src/main/java/ru/hehmdalolkek/blog/config/MvcConfig.java
@@ -10,6 +10,7 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/logout").setViewName("logout");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
